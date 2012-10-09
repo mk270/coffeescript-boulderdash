55a88b49955d10f020867ebcd03de4d65128a5d2
Martin Keegan <martin@no.ucant.org>
2012-10-09 02:18:16 +0100

    clean up some more of the class glue

--- boulderdash.js
+++ boulderdash.js
@@ -22,11 +22,9 @@ Object.prototype.extend = function(source) {
     return this;
 };
 
-// key, dom, dir, object, point, game, render, loop
-
 BD = {};
+
 BD.extend({
-  KEY: { ENTER: 13, ESC: 27, SPACE: 32, PAGEUP: 33, PAGEDOWN: 34, LEFT: 37, UP: 38, RIGHT: 39, DOWN: 40 },
   Dom: {
 	get:     function(id)       { return (id instanceof HTMLElement) ? id : document.getElementById(id); },
 	set:     function(id, html) { BD.Dom.get(id).innerHTML = html; },
@@ -691,7 +689,7 @@ Boulderdash = function() {
   // GENERAL purpose constants and helper methods
   //=========================================================================
 
-  var KEY = BD.KEY;
+  var KEY = { ENTER: 13, ESC: 27, SPACE: 32, PAGEUP: 33, PAGEDOWN: 34, LEFT: 37, UP: 38, RIGHT: 39, DOWN: 40 };
 
   var Dom = BD.Dom;
 
